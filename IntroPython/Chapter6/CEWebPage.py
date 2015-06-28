7dc76c531a606f27e111737231a29d942a554193
James Elder <james.wesley.elder@gmail.com>
2015-06-28 15:21:35 -0600

    test

---
+++ IntroPython/Chapter6/CEWebPage.py
@@ -0,0 +1,14 @@
+'''
+Created on Jun 28, 2015
+
+@author: jelder
+'''
+
+class CEWebPage():
+
+    def __init__(self, url):
+        self.url = url
+        
+    def print_url(self):
+        print(self.url)
+    
---
+++ IntroPython/Chapter6/__init__.py
@@ -0,0 +1,10 @@
+'''
+Created on Jun 28, 2015
+
+@author: jelder
+'''
+from Chapter6.CEWebPage import CEWebPage
+
+if __name__ == '__main__':
+    page = CEWebPage("/")   
+    page.print_url()
